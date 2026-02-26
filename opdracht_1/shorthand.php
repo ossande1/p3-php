11f519d33e4cedc6ae249e808e865031999153a1
Ossande Gima Da silva <9030460@student.zadkine.nl>
2026-02-26 15:00:53 +0100

    Add files via upload

---
+++ opdracht_1/shorthand.php
@@ -0,0 +1,21 @@
+<?php
+
+$naam = "Ossande";
+$leeftijd = "18";
+$programmertaal = "php"
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>Hallo <?= $naam ?></div>
+    <div>Je bent <?= $leeftijd ?> jaar oud</div>
+    <div>Jouw favoriete programmertaal is <?= $programmertaal ?></div>
+</body>
+</html>
