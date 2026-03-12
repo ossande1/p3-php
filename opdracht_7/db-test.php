df4c0792abe45f081c9718488d95f126052e4c5c
Ossande Gima Da silva <9030460@student.zadkine.nl>
2026-03-12 13:59:23 +0100

    Add files via upload

---
+++ opdracht_7/db-test.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "test";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Database connectie gelukt!";
+} catch(PDOException $e) {
+  echo "Database connectie mislukt." . $e->getMessage();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
