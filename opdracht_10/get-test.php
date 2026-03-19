85cbf3a6229aad692ca6a79a0fb3171d5d97efc0
Ossande Gima Da silva <9030460@student.zadkine.nl>
2026-03-19 13:41:33 +0100

    Add files via upload

---
+++ opdracht_10/get-test.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="POST" action="verwerk.php">
+    <label for="title">Titel</label>
+    <input id="title" name="title" type="text">
+    <button type="submit">Opslaan</button>
+    <?php $id = $_GET['title'] ?? '';
+    echo $id; ?>
+</form>
+</body>
+</html>
