b78f9c70f33e34f7c4214ecfb512e76f536a4c44
Ossande Gima Da silva <9030460@student.zadkine.nl>
2026-06-04 13:50:54 +0200

    Add files via upload

---
+++ opdracht_29/index.php
@@ -0,0 +1,14 @@
+<?php
+$id = 1;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="delete.php?id=<?= $id['id'] ?>">Verwijderen</a>
+</body>
+</html>
